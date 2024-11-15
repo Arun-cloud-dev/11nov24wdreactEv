6a42f9f589353e2bc51655825fb65ae9445ffb0b
Arun raj <riderarunraj@live.com>
2024-11-15 15:46:02 +0530

    update

---
+++ arun-test-vite/src/App.jsx
@@ -0,0 +1,20 @@
+
+
+
+function App() {
+ 
+  return (
+    <>
+        <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+<button 
+onClick={}
+    className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-red-600 transition duration-300">
+Button 
+  </button>
+    </>
+  )
+}
+
+export default App
